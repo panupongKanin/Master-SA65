582e4584c2cc832fecdcb1126321ccfb94f07cdf
PattanasakGit <111237717+PattanasakGit@users.noreply.github.com>
2022-10-23 12:42:23 +0700

    first comment

--- Backend/main.go
+++ Backend/main.go
@@ -1,19 +1,18 @@
 package main
- 
+
 import (
-  "github.com/panupongKanin/ProjectSA-arm/controller"
-  "github.com/panupongKanin/ProjectSA-arm/entity"
-  "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
+	"github.com/panupongKanin/ProjectSA-arm/controller"
+	"github.com/panupongKanin/ProjectSA-arm/entity"
 )
 
 func main() {
-  	entity.SetupDatabase()
+	entity.SetupDatabase()
 
-
-  	r := gin.Default()
+	r := gin.Default()
 	r.Use(CORSMiddleware())
 
-// ================= 01 ========================================================================================= 
+	// ================= 01 =========================================================================================
 	r.GET("/GetUser", controller.GetUser)
 	r.POST("/CreateUser", controller.CreateUser)
 
@@ -33,55 +32,51 @@ func main() {
 	r.GET("/GetPatient", controller.GetPatient)
 	r.POST("/CreatePatient", controller.CreatePatient)
 
-
-
 	//=========== Main Table Mapping Bed ===========
-	r.POST("/CreateMapBed",controller.CreateMapBed)
-	r.GET("/GetListMapBeds",controller.GetListMapBeds)
-	r.GET("/GetMapBed/:id",controller.GetMapBed)
+	r.POST("/CreateMapBed", controller.CreateMapBed)
+	r.GET("/GetListMapBeds", controller.GetListMapBeds)
+	r.GET("/GetMapBed/:id", controller.GetMapBed)
 	//=========== Main Table Mapping Bed ===========
 
 	//===========Zone===========
-	r.POST("/CreateZone",controller.CreateZone)
-	r.GET("/GetListZones",controller.ListZones)
+	r.POST("/CreateZone", controller.CreateZone)
+	r.GET("/GetListZones", controller.ListZones)
 	//===========Bed===========
-	r.POST("/CreateBed",controller.CreateBed)
-	r.GET("/GetListBeds",controller.ListBeds)
-	r.GET("/Bed/:zoneid",controller.GetBed_by_zone)
-	r.GET("/GetBedName/:id",controller.GetBedName)
+	r.POST("/CreateBed", controller.CreateBed)
+	r.GET("/GetListBeds", controller.ListBeds)
+	r.GET("/Bed/:zoneid", controller.GetBed_by_zone)
+	r.GET("/GetBedName/:id", controller.GetBedName)
 
 	r.PATCH("/UpdateBedstate", controller.UpdateBedstate)
 	r.PATCH("/UpdateTriagestate", controller.UpdateTriagestate)
 
-
 	//===========IPD===========
-	r.POST("/CreateIPD",controller.CreateIPD)
-	r.GET("/GetListIPDs",controller.GetListIPDs)
-	r.GET("/GetIPD/:id",controller.GetIPD)
+	//dhfsjkfskf
+	r.POST("/CreateIPD", controller.CreateIPD)
+	r.GET("/GetListIPDs", controller.GetListIPDs)
+	r.GET("/GetIPD/:id", controller.GetIPD)
 
-	
 	//===========DiseaseType===========
-	r.POST("/CreateDiseaseType",controller.CreateDiseaseType)
-	r.GET("/GetListDiseaseType",controller.GetListDiseaseTypes)
-	r.GET("/GetDiseaseType/:id",controller.GetDiseaseType)
+	r.POST("/CreateDiseaseType", controller.CreateDiseaseType)
+	r.GET("/GetListDiseaseType", controller.GetListDiseaseTypes)
+	r.GET("/GetDiseaseType/:id", controller.GetDiseaseType)
 
 	//===========Disease===========
-	r.POST("/CreateDisease",controller.CreateDisease)
-	r.GET("/GetListDisease",controller.GetListDiseases)
-	r.GET("/GetDisease/:id",controller.GetDisease)
+	r.POST("/CreateDisease", controller.CreateDisease)
+	r.GET("/GetListDisease", controller.GetListDiseases)
+	r.GET("/GetDisease/:id", controller.GetDisease)
 
 	//===========Triage===========
-	r.POST("/CreateTriage",controller.CreateTriage)
-	r.GET("/GetListTriages",controller.GetListTriages)
-	r.GET("/GetTriage/:id",controller.GetTriage)
-
+	r.POST("/CreateTriage", controller.CreateTriage)
+	r.GET("/GetListTriages", controller.GetListTriages)
+	r.GET("/GetTriage/:id", controller.GetTriage)
 
 	r.POST("/signup", controller.CreateUser)
 	// login User Route
 	r.POST("/login", controller.Login)
-	r.GET("/user/:id",controller.GetUser)
- 
-  	r.Run("localhost:8080")
+	r.GET("/user/:id", controller.GetUser)
+
+	r.Run("localhost:8080")
 }
 
 func CORSMiddleware() gin.HandlerFunc {
@@ -90,12 +85,12 @@ func CORSMiddleware() gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT,PATCH")
-     
+
 		if c.Request.Method == "OPTIONS" {
-		  c.AbortWithStatus(204)
-		  return
+			c.AbortWithStatus(204)
+			return
 		}
-     
+
 		c.Next()
 	}
 }
